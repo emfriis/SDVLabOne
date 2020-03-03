6705be5749c569a106fb7cd2ca2059f148f476f9
Emily Friis <emily-friis@live.nmit.ac.nz>
2020-03-03 15:02:07 +1300

    Added console.log code and documentation.

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ var numFirst = 34; // Assigns value 34 to variable numFirst.
 var numSecond = 25; // Assigns value 25 to variable numSecond.
 
 var numSum = numFirst + numSecond; // Assigns value to numSum equal to first number plus the second number.
+console.log(numSum); // Prints value of numSum.
 
 var numSub = numFirst - numSecond; // Assigns value to numSub equal to first number minus the second number.
+console.log(numSub); // Prints value of numSub.
 
 var numMult = numFirst * numSecond; // Assigns value to numMult equal to first number multiplied by the second number.
+console.log(numMult); // Prints value of numMult.
 
 var numDiv = numFirst / numSecond; // Assigns value to numDiv equal to first number divided the second number.
+console.log(numDiv); // Prints value of numDiv.
 
 var numRem = numFirst / numSecond; // Assigns value to numRem equal to the remainder of the first number divided by the second number.
+console.log(numRem); // Prints value of numRem.
 
-var numCalc = numFirst - numFirst / numSecond + numSecond * numFirst % numSecond; // Assigns value to numCalc equal to an operation with multiple methods involving the first and second numbers.
+var numCalc = numFirst - (numFirst / numSecond) + (numSecond * (numFirst % numSecond)); // Assigns value to numCalc equal to an operation with multiple methods involving the first and second numbers.
+console.log(numCalc); // Prints value of numCalc.
