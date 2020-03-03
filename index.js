6a25906ff34ced62f7ea3e498a32eaa2c86d3e2f
Emily Friis <emily-friis@live.nmit.ac.nz>
2020-03-03 13:25:00 +1300

    Added code and documentation

---
+++ index.js
@@ -0,0 +1,14 @@
+var numFirst = 34; // Assigns value 34 to variable numFirst.
+var numSecond = 25; // Assigns value 25 to variable numSecond.
+
+var numSum = numFirst + numSecond; // Assigns value to numSum equal to first number plus the second number.
+
+var numSub = numFirst - numSecond; // Assigns value to numSub equal to first number minus the second number.
+
+var numMult = numFirst * numSecond; // Assigns value to numMult equal to first number multiplied by the second number.
+
+var numDiv = numFirst / numSecond; // Assigns value to numDiv equal to first number divided the second number.
+
+var numRem = numFirst / numSecond; // Assigns value to numRem equal to the remainder of the first number divided by the second number.
+
+var numCalc = numFirst - numFirst / numSecond + numSecond * numFirst % numSecond; // Assigns value to numCalc equal to an operation with multiple methods involving the first and second numbers.
